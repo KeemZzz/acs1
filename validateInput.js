819a18f4d53acfd5f843c87c9ad30297eab79ee6
KeemZzz <144158682+KeemZzz@users.noreply.github.com>
2023-12-12 00:12:52 -0700

    Update validateInput.js

--- validateInput.js
+++ validateInput.js
@@ -2,37 +2,44 @@ function charAtDemo() {
     var myText = "Comm,Separated,Values";
     var indexOfFirstComma = myText.indexOf(",");
     console.log(indexOfFirstComma);
-    //document.getElementById("charAt").innerHTML = sum;
+    
 }
 
-
-
-function validateInput(event){
+function validateInput(event) {
     event.preventDefault();
+
     let firstName = document.getElementById("fname").value;
     console.log(firstName);
+
     let lastName = document.getElementById("lname").value;
     console.log(lastName);
+
     let zip = document.getElementById("zip").value;
     console.log(zip);
-    //concentrate first and last name variables
+
+   
     let firstLast = firstName + " " + lastName;
     console.log(firstLast);
-    if(firstLast.length < 20){
+
+    
+    if (firstLast.length < 20) {
         alert("Not enough characters. Your first and last name must have at least 20 characters.");
         return false;
     }
-    // check if zip has 5 characters and is a number.
-    if(zip.length != 5 || (!Number.isInteger(parseInt(zip)) && parseInt(zip) > 9999)){
-        alert.apply("Invalid zip code. Zip must be a number of 5 digits.");
-        console.log(typeof zip)
+
+    
+    if (zip.length !== 5 || isNaN(zip)) {
+        alert("Invalid zip code. Zip must be a number of 5 digits.");
         return false;
     }
+
     console.log("Validated!");
 }
+
+
 window.addEventListener("DOMContentLoaded", (event) => {
     const form = document.getElementById("myForm");
-    if (form){
-        form.addEventListener("Submit", validateInput);
+    if (form) {
+        form.addEventListener("submit", validateInput); 
     }
-});
+})
